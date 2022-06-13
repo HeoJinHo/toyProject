168c0d26642ed1568c83a307b8008d390b7bef0e
허진호 <hjh701234@gmail.com>
2022-06-13 16:57:46 +0900

    게시판 등록 예제

---
+++ src/main/java/com/web/board/config/ParameterStoreProperties.java
@@ -0,0 +1,19 @@
+package com.web.board.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Configuration
+public class ParameterStoreProperties {
+
+    @Value("${encrypt.key}")
+    private String encryptKey;
+
+
+}
--- src/test/java/com/web/board/service/TestRestApiServiceTest.java
+++ src/test/java/com/web/board/service/TestRestApiServiceTest.java
@@ -7,10 +7,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
-import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
@@ -41,38 +39,7 @@ class TestRestApiServiceTest {
     }
 
 
-    @Test
-    @Rollback(value = false)
-    @DisplayName("카테고리 등록")
-    public void categorySave(){
-        //given
-        Category category01 = new Category();
-        category01.setName("test01");
-        Category save01 = categoryRepository.save(category01);
 
-        Category category02 = new Category();
-        category02.setName("test02");
-        category02.setParent(save01);
-        Category category03 = new Category();
-        category03.setName("test03");
-        category03.setParent(save01);
-
-
-        Category save02 = categoryRepository.save(category02);
-        Category save03 = categoryRepository.save(category03);
-
-
-
-
-        category01.setChild(Arrays.asList(save02, save03));
-
-
-
-        //when
-
-        //the
-
-    }
 
 
 }
